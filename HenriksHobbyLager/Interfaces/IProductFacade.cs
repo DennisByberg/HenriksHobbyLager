fecf5270d7ed007bf235a6f479fdcb689bb9aec6
94denbyb <94denbyb@gafe.molndal.se>
2024-11-30 22:53:35 +0100

    feat/refactor: Flytta logik från Main() till separata klasser och skapa hjälparklass för utskrifter

---
+++ HenriksHobbyLager/Interfaces/IProductFacade.cs
@@ -0,0 +1,12 @@
+﻿namespace HenriksHobbyLager.Interfaces
+{
+    public interface IProductFacade
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProduct(int id);
+        void CreateProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+        IEnumerable<Product> SearchProducts(string searchTerm);
+    }
+}
